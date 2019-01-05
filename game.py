a9ff339cbbe7e662f169df1660146043d3e5958c
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2019-01-05 15:19:33 -0200

    + Adding more info about the game and create a new class

---
+++ game.py
@@ -0,0 +1,6 @@
+class Game:
+    def __init__(self, name, category, console):
+        self.name = name
+        self.category = category
+        self.console = console
+
--- handler.py
+++ handler.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
 
+from game import Game
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_flask():
-    games: list = ['Super Mario', 'The Legend Of Zelda', 'Tetris']
+
+    mario = Game(name='Super Mario', category='Classic', console='SNES')
+    zelda = Game(name='The Legend Of Zelda', category='RPG', console='WII')
+
+    games: list = [mario, zelda]
     return render_template("index.html", page_name='Jogos', games=games)
 
 
