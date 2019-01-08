0e8dbb16e67f720353a1d6924c2f8fd3c33508f8
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2019-01-08 08:32:05 -0200

    + Adding new route to add a new game

--- handler.py
+++ handler.py
@@ -1,18 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from game import Game
 
 app = Flask(__name__)
 
+mario = Game(name='Super Mario', category='Classic', console='SNES')
+zelda = Game(name='The Legend Of Zelda', category='RPG', console='WII')
+
+games: list = [mario, zelda]
+
 
 @app.route("/")
-def hello_flask():
-
-    mario = Game(name='Super Mario', category='Classic', console='SNES')
-    zelda = Game(name='The Legend Of Zelda', category='RPG', console='WII')
-
-    games: list = [mario, zelda]
+def index():
     return render_template("index.html", page_name='Jogos', games=games)
 
 
-app.run()
+@app.route("/new_game")
+def new_game():
+    return render_template("new_game.html", page_name="Novo Jogo")
+
+
+@app.route("/save_game", methods=['POST'])
+def save_game():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    new_game = Game(name=nome, category=categoria, console=console)
+    games.append(new_game)
+
+    return render_template("index.html", page_name='Jogos', games=games)
+
+
+app.run(debug=True)
