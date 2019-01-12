1fca34f9fc8d87dfad31e941fb2dfbb3aaec4856
Gabrielly de Andrade <gabrielly.andrade@poli.ufrj.br>
2019-01-12 00:56:32 -0200

    + Adding login screen, storage data in session and logout

--- handler.py
+++ handler.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 from game import Game
 
 app = Flask(__name__)
+app.secret_key = "flask"
 
 mario = Game(name='Super Mario', category='Classic', console='SNES')
 zelda = Game(name='The Legend Of Zelda', category='RPG', console='WII')
@@ -30,6 +31,30 @@ def save_game():
     games.append(new_game)
 
     return redirect("/")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html", page_name="Login")
+
+
+@app.route("/logout")
+def logout():
+    session["is_logged"] = None
+    flash("Nenhum usuário logado!")
+    return redirect("/")
+
+
+@app.route("/auth", methods=['POST'])
+def auth():
+    default = "mestra"
+    session["is_logged"] = request.form["senha"]
+    if default == session["is_logged"]:
+        flash(f"{request.form['usuario']} logado com sucesso")
+        return redirect("/")
+    else:
+        flash("Tente novamente!")
+        return redirect("login")
 
 
 app.run(debug=True)
